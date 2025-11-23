98d09f7988a0f6e826b5f85830d71b4f84a27ab7
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:49:36 +0900

    feat: FavoriteRepository 초기 구현(기본 함수 구조 설정)

---
+++ app/src/main/java/com/konkuk/ottae/favorite/FavoriteRepository.kt
@@ -0,0 +1,17 @@
+package com.konkuk.ottae.favorite
+
+class FavoriteRepository {
+
+    fun getFavorites(): List<FavoriteEntity> {
+
+        return emptyList()
+    }
+
+    fun addFavorite(item: FavoriteEntity) {
+
+    }
+
+    fun removeFavorite(item: FavoriteEntity) {
+
+    }
+}
