219b3187b5d5c578af083350415203c135a60c90
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:50:08 +0900

    feat: BusCategoryActivity 버튼 클릭 로직 추가(초기 Toast)

--- app/src/main/java/com/konkuk/ottae/bus/BusCategoryActivity.kt
+++ app/src/main/java/com/konkuk/ottae/bus/BusCategoryActivity.kt
@@ -2,11 +2,24 @@ package com.konkuk.ottae.bus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.konkuk.ottae.R
 
 class BusCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bus_category)
 
-        setContentView(android.R.layout.simple_list_item_1)
+        val btnFront = findViewById<Button>(R.id.btnFront)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnFront.setOnClickListener {
+            Toast.makeText(this, "정문 버스 정보 준비중", Toast.LENGTH_SHORT).show()
+        }
+
+        btnBack.setOnClickListener {
+            Toast.makeText(this, "후문 버스 정보 준비중", Toast.LENGTH_SHORT).show()
+        }
     }
 }
