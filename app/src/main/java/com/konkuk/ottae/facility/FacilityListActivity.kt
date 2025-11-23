1ab1862c1ef488f77a49ded86b7948af475e352d
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:56:10 +0900

    feat: FacilityListActivity 더미 데이터 기반 리스트 표시 기능 추가

--- app/src/main/java/com/konkuk/ottae/facility/FacilityListActivity.kt
+++ app/src/main/java/com/konkuk/ottae/facility/FacilityListActivity.kt
@@ -9,7 +9,7 @@ import com.konkuk.ottae.R
 class FacilityListActivity : AppCompatActivity() {
 
     private lateinit var recycler: RecyclerView
-    private val adapter = FacilityAdapter()
+    private lateinit var adapter: FacilityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +17,16 @@ class FacilityListActivity : AppCompatActivity() {
 
         recycler = findViewById(R.id.recyclerFacilities)
         recycler.layoutManager = LinearLayoutManager(this)
+
+        // 개발 단계: 더미 데이터
+        val dummyList = listOf(
+            Facility("스타벅스", "카페"),
+            Facility("홍콩반점", "중식"),
+            Facility("오코노미야끼", "일식"),
+            Facility("맘스터치", "버거")
+        )
+
+        adapter = FacilityAdapter(dummyList)
         recycler.adapter = adapter
     }
 }
