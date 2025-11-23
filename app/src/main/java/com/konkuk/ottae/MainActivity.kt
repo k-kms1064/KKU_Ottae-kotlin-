578a4cafe13d6a5757ccd0f3c68dbcad140089d4
k-kms1064 <kms106418@gmail.com>
2025-11-23 18:54:47 +0900

    refactor: MainActivity 코드 구조 정리

--- app/src/main/java/com/konkuk/ottae/MainActivity.kt
+++ app/src/main/java/com/konkuk/ottae/MainActivity.kt
@@ -1,36 +1,33 @@
 package com.konkuk.ottae
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.konkuk.ottae.bus.BusCategoryActivity
-import com.konkuk.ottae.databinding.ActivityMainBinding
 import com.konkuk.ottae.facility.FacilityCategoryActivity
 import com.konkuk.ottae.favorite.FavoriteListActivity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.konkuk.ottae.bus.BusCategoryActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val btnBus = findViewById<LinearLayout>(R.id.btnBus)
+        val btnFacility = findViewById<LinearLayout>(R.id.btnFacility)
+        val btnFavorites = findViewById<LinearLayout>(R.id.btnFavorites)
 
-        binding.busCard.setOnClickListener {
-            val intent = Intent(this, BusCategoryActivity::class.java)
-            startActivity(intent)
+        btnBus.setOnClickListener {
+            startActivity(Intent(this, BusCategoryActivity::class.java))
         }
 
-        binding.facilityCard.setOnClickListener {
-            val intent = Intent(this, FacilityCategoryActivity::class.java)
-            startActivity(intent)
+        btnFacility.setOnClickListener {
+            startActivity(Intent(this, FacilityCategoryActivity::class.java))
         }
 
-        binding.favoriteCard.setOnClickListener {
-            val intent = Intent(this, FavoriteListActivity::class.java)
-            startActivity(intent)
+        btnFavorites.setOnClickListener {
+            startActivity(Intent(this, FavoriteListActivity::class.java))
         }
     }
 }
