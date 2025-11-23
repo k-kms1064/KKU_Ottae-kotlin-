74221baaae1acac6480e072e8e64325e94596312
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:50:33 +0900

    feat: FacilityListActivity RecyclerView 초기 연결

--- app/src/main/java/com/konkuk/ottae/facility/FacilityListActivity.kt
+++ app/src/main/java/com/konkuk/ottae/facility/FacilityListActivity.kt
@@ -2,11 +2,21 @@ package com.konkuk.ottae.facility
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.konkuk.ottae.R
 
 class FacilityListActivity : AppCompatActivity() {
+
+    private lateinit var recycler: RecyclerView
+    private val adapter = FacilityAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facility_list)
 
-        setContentView(android.R.layout.simple_list_item_1)
+        recycler = findViewById(R.id.recyclerFacilities)
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.adapter = adapter
     }
 }
