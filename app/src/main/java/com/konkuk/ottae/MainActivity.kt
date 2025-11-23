bcfc2eccea581f33377c3852fc6ae046d5c9a6ef
k-kms1064 <kms106418@gmail.com>
2025-11-23 18:35:34 +0900

    refactor: MainActivity 리팩토링 및 UI 연결 수정

--- app/src/main/java/com/konkuk/ottae/MainActivity.kt
+++ app/src/main/java/com/konkuk/ottae/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.konkuk.ottae
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.konkuk.ottae.bus.BusCategoryActivity
+import com.konkuk.ottae.databinding.ActivityMainBinding
+import com.konkuk.ottae.facility.FacilityCategoryActivity
+import com.konkuk.ottae.favorite.FavoriteListActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(android.R.layout.simple_list_item_1)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.busCard.setOnClickListener {
+            val intent = Intent(this, BusCategoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.facilityCard.setOnClickListener {
+            val intent = Intent(this, FacilityCategoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.favoriteCard.setOnClickListener {
+            val intent = Intent(this, FavoriteListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
