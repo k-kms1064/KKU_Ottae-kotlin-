64b255da4654e46d273f6f75f7ca12164532e3ba
k-kms1064 <kms106418@gmail.com>
2025-11-23 17:59:09 +0900

    feat: FavoriteRepository Room DB 사용 준비 구조 추가

--- app/src/main/java/com/konkuk/ottae/favorite/FavoriteRepository.kt
+++ app/src/main/java/com/konkuk/ottae/favorite/FavoriteRepository.kt
@@ -1,9 +1,20 @@
 package com.konkuk.ottae.favorite
 
-class FavoriteRepository {
+import android.content.Context
+import androidx.room.Room
+import com.konkuk.ottae.AppDatabase
+
+class FavoriteRepository(context: Context) {
+
+    private val db: AppDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class.java,
+        "favorite-db"
+    ).build()
+
+    private val dao = db.favoriteDao()
 
     fun getFavorites(): List<FavoriteEntity> {
-
         return emptyList()
     }
 
